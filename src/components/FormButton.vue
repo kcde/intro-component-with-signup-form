67bfbc1ddc0edbe74c29b9d7cade94200a50fcd2
keside <wilsonezeala@yahoo.com>
2022-09-04 23:49:39 +0100

    add submit button hover effect

--- src/components/FormButton.vue
+++ src/components/FormButton.vue
@@ -6,16 +6,21 @@
 export default {};
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 button {
   width: 100%;
-  background-color: var(--bg-submit-btn);
+  background-color: rgba(var(--bg-submit-btn));
   color: var(--clr-white);
   padding: 1.25rem 1rem;
   text-transform: uppercase;
   font-weight: 500;
   border-radius: var(--radius-md);
-  box-shadow: inset 0 -4px rgba(var(--clr-dark-blue), 0.1);
+  box-shadow: inset 0 -4px rgba(var(--clr-dark-blue), 0.2);
+  transition: background-color 150ms ease-out;
   cursor: pointer;
+
+  &:hover {
+    background-color: rgba(var(--bg-submit-btn), 0.6);
+  }
 }
 </style>
